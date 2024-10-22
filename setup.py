19b212939d51d23054a1ca49054663a543c64dcf
shreyashettyu <shreyashettyu2@gmail.com>
2024-10-23 00:11:05 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="demand-forecast",
+    version="0.0.0",
+    author="shreya",
+    author_email="shreyashettyu2@gmail.com",
+    packages=find_packages()
+)
